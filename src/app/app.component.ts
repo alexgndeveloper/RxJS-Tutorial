c06a9a8cf5dda2bb853ee5df31a608ebd5afb9c1
Alejandro Garcia <alexgn26developer@gmail.com>
2021-01-21 21:57:58 +0100

    Interval y Timer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,87 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  /**
+   * Titulo de la aplicacion
+   */
   public title = 'Tutorial-RxJS';
+  /**
+   * Informacion del Observable
+   */
+  public info = '';
+  /**
+   * Resultado que muestra la suscripcion
+   */
+  public result = '';
+  /**
+   *  Suscripcion
+   */
+  public subcription: Subscription = new Subscription();
+  /**
+   * Lista de Observables
+   */
+  public observables = [
+    {
+      name: 'interval',
+      info:
+        'El operador interval se ejecuta cada vez hasta el infinito o nos desuscribamos, el perido de tiempo que le ' +
+        'hayamos asignado en este caso 1 segundo (1000 milisegundos)',
+    },
+    {
+      name: 'timer',
+      info:
+        'Este observable se ejecuta pasado el tiempo que le hemos asignado en este caso 3 segundos.',
+    },
+    { name: 'fromEvent', info: '' },
+  ];
+
+  /**
+   * observableMethods
+   */
+  public observableMethods(method: string, info: string): void {
+    this.unsubcribe();
+    this.info = info;
+
+    switch (method) {
+      case 'interval':
+        this.methodInterval();
+        break;
+      case 'timer':
+        this.methodTimer();
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  /**
+   * methodTimer. Creamos un observable de la variable Timer.
+   */
+  private methodTimer(): void {
+    const contador = timer(3000);
+    this.subcription = contador.subscribe(() => {
+      this.result = '------------> Timer <------------';
+    });
+  }
+
+  /**
+   * methodInterval. Creamos un observable de la variable Interval.
+   */
+  private methodInterval(): void {
+    const contador = interval(1000);
+
+    this.subcription = contador.subscribe((n) => {
+      this.result += `Cada ${n} segundos \n`;
+    });
+  }
+
+  /**
+   * unsubcribe. Vaciamos la informacion de la consola y nos desuscribimos.
+   */
+  private unsubcribe(): void {
+    this.info = '';
+    this.result = '';
+    this.subcription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
